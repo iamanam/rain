94423f357129822e2ada4b28be8c91b25372d15a
iamanam <iamanam4@gmail.com>
2016-02-24 22:45:23 +0600

    some minor changes and result page add

--- client/views/rain/rain.js
+++ client/views/rain/rain.js
@@ -1,7 +1,16 @@
 if (Meteor.isClient) {
-    var question = ["whats your name", "From the very first day did u like me?", "ok"];
+    var question = ["Do you know my name? If you do what is it?", "From the very first day did you like me?",
+        "Were you honest with everything you said about yourself", "How much you trust me (in percentage-%)?"];
+    var possibleAnswer = [["hasan", "anamul", "error", "korim"], ["yes", "never", "after a while"], ["yes", "most of the time", "sometime", "no"]];
+    var step = new ReactiveVar(1);
 
+    Template.registerHelper("step", () => {
+        return step.get();
+    });
     Template.pic.onRendered(function () {
+        Session.set("anim_stop", false);
+        Session.set("resultShow", false);
+
         var self = this;
         var image = document.getElementById('background');
         image.crossOrigin = 'anonymous';
@@ -36,56 +45,84 @@ if (Meteor.isClient) {
 
 }
 
+Template.rain.helpers({
+    showResult: function () {
+        if (Session.get("resultShow"))
+            return Session.get("resultShow");
+    }
+});
+Template.rain.onCreated = function () {
+    this.step = 1;
+};
 function getQuestion(counter) {
     return question[counter];
 }
 
-var counter = new ReactiveVar(0);
+Template.quiz.created = function () {
+    this.answerSet = [];
+    this.answer = new ReactiveVar();
+    this.counter = new ReactiveVar(0);
+};
 
 Template.quiz.helpers({
-    question: function (p) {
+    question: function () {
         if (Session.get("anim_stop")) {
-            var template =$(".quiz");
+            var tmpl = Template.instance();
+            var template = tmpl.$(".quiz");
             $("#animation").hide();
             template.fadeIn(1000);
-            var q = getQuestion(counter.get());
+            var q = getQuestion(tmpl.counter.get());
             if (q) {
                 return q;
             }
             else {
                 template.fadeOut(1000);
                 var associative_array = new Object();
+                var result = [];
                 for (var i = 0; i < 3; i++) {
-                    associative_array[question[i]] = answer[i];
+                    associative_array[question[i]] = tmpl.answerSet[i];
                 }
-                console.log(associative_array);
+                Session.set("resultShow", true);
+                // Blaze.render(Template.resultshow, document.getElementsByClassName("quiz"));
+                //console.log(Template.parentData());
+
             }
+        }
+    },
+    possibleAns: function () {
+        if (Session.get("anim_stop")) {
+            var op = possibleAnswer[Template.instance().counter.get()];
+            return op || false;
         }
     }
 });
 
-var answer = [];
 
 Template.quiz.events({
     "submit .question": function (e, t) {
         e.preventDefault();
-        var value = t.$(".ans").val();
+        var value = t.answer.get() === undefined ? t.$(".ans").val() : t.answer.get();
         t.$(".ans").val("");
         if (value && value !== undefined && value !== null) {
             Template.instance().$(".error").slideUp(500);
             var step = $(".step");
-            step.eq(counter.get()).removeClass(".step-unfinish").addClass("step-finish");
-            answer.push(value);
-            counter.set(counter.get() + 1);
+            step.eq(t.counter.get()).removeClass(".step-unfinish").addClass("step-finish");
+            t.answerSet.push(value);
+            t.counter.set(t.counter.get() + 1);
         }
         else {
             Template.instance().$(".error").slideDown(500);
         }
+    },
+    "click li": function (e, t) {
+        var ans = $(e)[0].currentTarget.innerText;
+        t.answer.set(ans);
+        $(".question").trigger("submit");
+        console.log(ans);
+
     }
 });
 
-Template.step.onRendered(function () {
-});
 
 Template.step.helpers({
     questionSet: function () {
