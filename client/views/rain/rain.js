61cc4fac7da1681fd6c78683769b0450e970a2f5
iamanam <iamanam4@gmail.com>
2016-02-24 22:45:23 +0600

    Major changes, Many optimazation, style added.

--- client/views/rain/rain.js
+++ client/views/rain/rain.js
@@ -1,94 +1,99 @@
 if (Meteor.isClient) {
-    var question = ["whats your name", "From the very first day did u like me?", "ok"];
+    var question = ["Do you know m?", "From the very first day did you like me?",
+        "Were you honest with everything you said about yourself", "How much you trust me (in percentage-%)?"];
+    var step = new ReactiveVar("quiz");
+    var answerSet = [];
+/////////////////////////////////////////----------------------------------------////////////////
+    /*
+     global helpers
+     */
 
-    Template.pic.onRendered(function () {
+    Template.rain.helpers({
+        "step": ()=> {
+            return step.get();
+        }
+    });
+
+    Template.welcome.onRendered(function () {
         var self = this;
         var image = document.getElementById('background');
-        image.crossOrigin = 'anonymous';
-        image.onload = function () {
+        var downloadImg = new Image();
+        downloadImg.onload = function () {
+            image.src = this.src;
             var engine = new RainyDay({
-                image: this,
-                // crop: [50, 50, 600, 400]
+                image: image,
+                crop: [50, 50, 600, 400]
             }, document.getElementById('canvas'));
             engine.trail = engine.TRAIL_SMUDGE;
             engine.rain([[3, 3, 0.1]], 33);
-            $(image).hide();
-            self.$(".loading-cat").hide();
-            Meteor.setTimeout(function () {
-                return Blaze.render(Template.text, document.getElementById("animation"));
-            }, 3000)
+            Meteor.setTimeout(()=> {
+                return Blaze.render(Template.text, document.getElementById("text-animation"));
+            }, 3000);
         };
-        image.src = '/images/1.jpg';
+        downloadImg.src = "/images/1.jpg";
     });
 
     Template.text.rendered = function () {
         $('h1.tlt').textillate({
-            initialDelay: 000
+            initialDelay: 1000
         });
         $('h2.tlt').textillate({
-            initialDelay: 000,
+            initialDelay: 4500,
             type: "char",
             callback: function () {
-                Session.set("anim_stop", true);
+                return step.set("quiz");
             }
-        }).delay(0000).fadeIn(1000)
+        })
     };
 
+
+    Template.quiz.created = function () {
+        this.answerSet = [];
+        this.counter = new ReactiveVar(0);
+    };
+
+    Template.quiz.helpers({
+        dataQ: ()=> {
+            return question.length - 1 >= Template.instance().counter.get() ?
+                question[Template.instance().counter.get()] : step.set("resultshow");
+        },
+        possibleAns: () => {
+            const optionAns = [["hasan", "anamul", "error", "korim"], ["yes", "never", "after a while"],
+                ["yes", "most of the time", "sometime", "no"]];
+            var op = optionAns[Template.instance().counter.get()];
+            return op || false;
+        }
+    });
+
+
+    Template.quiz.events({
+        "submit .question, click li": function (event, tem) {
+            event.preventDefault();
+            const qusAns = tem.$(".ans").val() !== undefined ? tem.$(".ans").val() : event.target.innerText;
+            answerSet.push(qusAns);//save the answer
+            //go to next step
+            tem.$(".step").eq(tem.counter.get()).removeClass(".step-unfinish").addClass("step-finish");
+            tem.counter.set(tem.counter.get() + 1);
+            console.log(answerSet);
+        }
+    });
+
+
+    Template.step.helpers({
+        questionSet: function () {
+            return question;
+        }
+    });
+
+    Template.resultshow.helpers({
+        getDetail(){
+
+            return answerSet;
+        },
+        getQus: function (index) {
+            return question[index]
+
+        }
+
+    })
 }
-
-function getQuestion(counter) {
-    return question[counter];
-}
-
-var counter = new ReactiveVar(0);
-
-Template.quiz.helpers({
-    question: function (p) {
-        if (Session.get("anim_stop")) {
-            var template =$(".quiz");
-            $("#animation").hide();
-            template.fadeIn(1000);
-            var q = getQuestion(counter.get());
-            if (q) {
-                return q;
-            }
-            else {
-                template.fadeOut(1000);
-                var associative_array = new Object();
-                for (var i = 0; i < 3; i++) {
-                    associative_array[question[i]] = answer[i];
-                }
-                console.log(associative_array);
-            }
-        }
-    }
-});
-
-var answer = [];
-
-Template.quiz.events({
-    "submit .question": function (e, t) {
-        e.preventDefault();
-        var value = t.$(".ans").val();
-        t.$(".ans").val("");
-        if (value && value !== undefined && value !== null) {
-            Template.instance().$(".error").slideUp(500);
-            var step = $(".step");
-            step.eq(counter.get()).removeClass(".step-unfinish").addClass("step-finish");
-            answer.push(value);
-            counter.set(counter.get() + 1);
-        }
-        else {
-            Template.instance().$(".error").slideDown(500);
-        }
-    }
-});
-
-Template.step.onRendered(function () {
-});
-
-Template.step.helpers({
-    questionSet: function () {
-        return question;
-    }
-});
